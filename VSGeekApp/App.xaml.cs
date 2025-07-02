cb6fa09c24a98eed8ea955ccd685d4c6531f6e8e
Samuel Juren <samuel.costa@onet-brasil.com.br>
2025-07-01 21:56:24 -0300

    Ajuste gerais para começo dos códigos.

--- VSGeekApp/App.xaml.cs
+++ VSGeekApp/App.xaml.cs
@@ -1,15 +1,12 @@
-﻿namespace VSGeekApp
+﻿using VSGeekApp.View.PaginaPrincipal;
+namespace VSGeekApp
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
-        }
-
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
+            MainPage =  new NavigationPage(new PaginaPrincipalPage());
         }
     }
 }
---
+++ VSGeekApp/View/PaginaPrincipal/PaginaPrincipalPage.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSGeekApp.View.PaginaPrincipal;
+
+public partial class PaginaPrincipalPage : TabbedPage
+{
+    public PaginaPrincipalPage()
+    {
+        InitializeComponent();
+    }
+}
